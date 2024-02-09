e7d917d3f9e48d5c2aa43a8aa2cff8ac22977735
Higor Soares <codehs07@gmail.com>
2024-02-08 21:43:36 -0300

    INIT

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+export const Header = () => {
+  return (
+    <>
+      <div>Teste</div>
+    </>
+  );
+};
---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+export default api;
